ccf710c1614a9b9620651a02167d73d5a8ddf968
Bhavan663 <venkateshbhavankalyan@gmail.com>
2023-08-21 10:31:22 +0530

    react

--- redbus/src/Components/Header/Header.jsx
+++ redbus/src/Components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 function Header(){
     return(
@@ -6,11 +8,14 @@ function Header(){
         <div className="nav-top" style={{width: "230px;"}}>
             <img style={{paddingTop:"20px",paddingLeft:"100px",marginBottom: "10px"}} src="https://st.redbus.in/Images/rdc/rdc-redbus-logo.svg" alt="redBus" className="rb_logo"/>
             <div className="nav-option" style={{marginLeft: "200px", alignItems: "center", display:"block", paddingTop: "15px"}}>
+           <Link to="View">
+           
             <div className="but">
-              <button onclick = "window.location.href='bussearch.html';" style={{borderColor: "transparent",backgroundColor: "transparent"}}>
+              <button style={{borderColor: "transparent",backgroundColor: "transparent"}}>
                 <img src="https://st.redbus.in/web/images/layout/rb_vertical.svg" alt="" /><p style={{width:"90px" ,paddingTop: "5px"}}>Bus Tickets</p>
                </button>
             </div>
+            </Link>
               
                </div>
           
@@ -36,8 +41,10 @@ function Header(){
 </div>
             </div>
         
-           
+              
     </div>
+
+    
     
     
     )
