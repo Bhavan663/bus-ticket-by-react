9f8265f28d1ae7a3247afd57a3e0102282fe9267
VijayLeonPaul <vijay14082002@gmail.com>
2023-08-21 10:31:22 +0530

    new

--- redbus/src/App.js
+++ redbus/src/App.js
@@ -1,17 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './Pages/Home';
-import View from './Pages/View';
-import {BrowserRouter,Route,Routes} from "react-router-dom"
+import Show from './Pages/Show';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-      <Routes>
-        
-      </Routes>
-      </BrowserRouter>
+      <Show />
     </>
   );
 }
